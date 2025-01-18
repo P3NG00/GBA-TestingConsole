2a9b1021a03816b43ccb2d8a14431740594bdfc0
P3NG00 <p3ng00minecraft@gmail.com>
2025-01-18 16:23:28 -0700

    Add vc_line function for cursor movement and enhance vc_print

--- source/console.c
+++ source/console.c
@@ -17,49 +17,51 @@
 //     }
 // }
 
-void vc_print(u8 x, u8 y, char text[]) {
-	// ansi escape sequence to set print co-ordinates
-	// /x1b[line;columnH
-	iprintf("\x1b[%d;%dH%s", x, y, text);
+void vc_print(u8 x, u8 y, const char *text) {
+    // ansi escape sequence to set print co-ordinates
+    // /x1b[line;columnH
+    iprintf("\x1b[%d;%dH%s", x, y, text);
+}
+
+enum VCLine {
+    LINE_UP     = 0,
+    LINE_DOWN  	= 1,
+    LINE_RIGHT 	= 2,
+    LINE_LEFT  	= 3,
+};
+
+void vc_line(enum VCLine direction, u8 n, const char *text) {
+    char controlChar = 'A' + (char)direction;
+    iprintf("\x1b[%d%c%s", n, controlChar, text);
 }
 
 int main(void) {
-	// the vblank interrupt must be enabled for VBlankIntrWait() to work
-	// since the default dispatcher handles the bios flags no vblank handler
-	// is required
-	irqInit();
-	irqEnable(IRQ_VBLANK);
+    // the vblank interrupt must be enabled for VBlankIntrWait() to work
+    // since the default dispatcher handles the bios flags no vblank handler
+    // is required
+    irqInit();
+    irqEnable(IRQ_VBLANK);
 
-	consoleInit(0, 4, 0, NULL, 0, 15);
+    consoleInit(0, 4, 0, NULL, 0, 15);
 
-	BG_COLORS[0] = RGB8(0b00010000, 0b00010000, 0b00010000);  // background color
-	BG_COLORS[241] = RGB5(0b11111, 0b11111, 0b11111);  // font color
+    // background color
+    BG_COLORS[0] = RGB8(0b00010000, 0b00010000, 0b00010000);
+    // font color
+    BG_COLORS[241] = RGB5(0b11111, 0b11111, 0b11111);
 
-	SetMode( MODE_0 | BG0_ON );
+    SetMode( MODE_0 | BG0_ON );
 
-	// ansi escape sequence to clear screen and home cursor
-	// /x1b[line;columnH (TODO this seems wrong)
-	iprintf("\x1b[2J");
+    // ansi escape sequence to clear screen and home cursor
+    // /x1b[line;columnH (#TODO this seems wrong)
+    iprintf("\x1b[2J");
 
-	vc_print(10, 10, "Hello World!");
+    vc_print(10, 10, "Hello World!");
+    vc_line(LINE_UP, 10, "Line 0");
+    vc_line(LINE_LEFT, 28, "Column 0");
+    vc_line(LINE_DOWN, 19, "Line 19");
+    vc_line(LINE_RIGHT, 5, "Column 20");
 
-	// ansi escape sequence to move cursor up
-	// /x1b[linesA
-	iprintf("\x1b[10ALine 0");
-
-	// ansi escape sequence to move cursor left
-	// /x1b[columnsD
-	iprintf("\x1b[28DColumn 0");
-
-	// ansi escape sequence to move cursor down
-	// /x1b[linesB
-	iprintf("\x1b[19BLine 19");
-
-	// ansi escape sequence to move cursor right
-	// /x1b[columnsC
-	iprintf("\x1b[5CColumn 20");
-
-	while (1) {
-		VBlankIntrWait();
-	}
+    while (1) {
+        VBlankIntrWait();
+    }
 }
