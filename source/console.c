44f4a12421d8695cc98d876bfd700f84c840dcf6
P3NG00 <p3ng00minecraft@gmail.com>
2025-01-18 22:11:32 -0700

    Implement input handling and refactor main loop for improved interaction

--- source/console.c
+++ source/console.c
@@ -1,22 +1,10 @@
 
 #include <gba_console.h>
-// #include <gba_input.h>
+#include <gba_input.h>
 #include <gba_interrupt.h>
 #include <gba_video.h>
 #include <gba_systemcalls.h>
 #include <stdio.h>
-
-
-// TODO use
-// bool button_held(u16 button) {
-//     /* and the button register with the button constant we want */
-//     u16 pressed = REG_KEYINPUT & button;
-//     if (pressed == 0) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
 
 
 #define CTRL "\x1b["
@@ -42,9 +30,74 @@ void vc_clear() {
 void vc_print(const char *text) {
     iprintf(text);
 }
+void vc_printc(char c) {
+    iprintf("%c", c);
+}
 
 
-int main(void) {
+char getCharacterFromHeld(u16 *_keysHeld) {
+    // initialize to checkable variable
+    u8 c = (u8)-1;
+
+    if (*_keysHeld & KEY_RIGHT) {
+        c = 2;    // 2
+        if (*_keysHeld & KEY_UP) {
+            c--;  // 1
+        } else if (*_keysHeld & KEY_DOWN) {
+            c++;  // 3
+        }
+    } else if (*_keysHeld & KEY_LEFT) {
+        c = 6;    // 6
+        if (*_keysHeld & KEY_UP) {
+            c++;  // 7
+        } else if (*_keysHeld & KEY_DOWN) {
+            c--;  // 5
+        }
+    } else if (*_keysHeld & KEY_UP) {
+        c = 0;    // 0
+    } else if (*_keysHeld & KEY_DOWN) {
+        c = 4;    // 4
+    }
+
+    // check if valid letter
+    if (c != (u8)-1) {
+        // check modifiers for other letters
+        if (*_keysHeld & KEY_L) {
+            c += 8;
+        } else if (*_keysHeld & KEY_R) {
+            c += 16;
+        }
+        // TODO implement currently unreachable 'y' and 'z' as current implementation only reaches 24 letters.
+        // TODO uppercase for a-z
+
+        return c + 'a';
+    }
+
+    return '-';  // TODO change to space
+}
+
+void handleInput() {
+    scanKeys();
+    u16 _keysHeld = keysHeld();
+    u16 _keysDown = keysDown();
+
+    // start clears screen and returns
+    if (_keysDown & KEY_START) {
+        vc_clear();
+        return;
+    }
+
+    // check typing key
+    if (_keysDown & KEY_A) {
+        char c = getCharacterFromHeld(&_keysHeld);
+        vc_printc(c);
+    } else if (_keysDown & KEY_B) {
+        // TODO backspace
+    }
+}
+
+
+int main() {
     /* the vblank interrupt must be enabled for VBlankIntrWait() to work since
      * the default dispatcher handles the bios flags no vblank handler is required */
     irqInit();
@@ -59,18 +112,11 @@ int main(void) {
 
     SetMode( MODE_0 | BG0_ON );
 
-    vc_clear();
-
-    // TODO remove testing
-    vc_print("Hello, World!");  // should be cleared by clear below
-    vc_clear();                 // should clear text above
-    vc_print("\ntesting");      // should print 'testing' on second line
-    vc_pos(0, 0);               // sets cursor to 0, 0
-    vc_print("whoah");          // should print 'whoah' on first line
-    vc_pos(1, 3);               // sets cursor to 1, 3
-    vc_print("yippee");         // should print 'yippee' on fourth line, 1 column from left
+    vc_print("\nwork in progress\n\nuse A to confirm letter\nuse Start to clear screen\nuse DPAD+LR combinations for different letters\n\n");  // TODO edit
 
     while (1) {
+        handleInput();
+
         VBlankIntrWait();
     }
 }
